9b6c4f3bd7afa1700fc54033aafb89a68b9c1497
Paolinodelca <p.delca63@gmail.com>
2025-12-12 12:48:39 +0100

    Change model from llama-3.1-70b-versatile to llama-3.1-8b-instant

--- api/chat.js
+++ api/chat.js
@@ -23,7 +23,8 @@ export default async function handler(req, res) {
         "Authorization": "Bearer " + apiKey
       },
       body: JSON.stringify({
-        model: "llama-3.1-70b-versatile",
+      //  model: "llama-3.1-70b-versatile",
+        model: "llama-3.1-8b-instant",
         messages: [
           {
             role: "system",
