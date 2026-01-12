4c36f9a9675810b11395e61deac3d20f01240dd0
Paolinodelca <p.delca63@gmail.com>
2026-01-12 15:17:34 +0100

    Update charles.js

--- api/charles.js
+++ api/charles.js
@@ -27,7 +27,10 @@ export default async function handler(req, res) {
     }
 
     const { playerText, gameState } = req.body;
-
+    //temporaneo
+  console.log("PLAYER TEXT:", playerText);
+  console.log("BODY:", req.body);
+//fine temporaneo
     // Stato di gioco
   /*
     const state = gameState || {
