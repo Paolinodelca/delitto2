3b00a483e99d45210976c0119afae561b8c8f02f
Paolino <p.delca63@gmail.com>
2026-02-27 09:06:14 +0100

    Add scenario switching via URL

--- api/observe.js
+++ api/observe.js
@@ -1,6 +1,14 @@
 export default async function handler(req, res) {
   console.log("OBSERVE VERSION: AMP-V6");
 
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+if (req.method === "OPTIONS") {
+  return res.status(200).end();
+}
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Solo POST consentito" });
   }
